57f5cff37f7ac6c87465d8db49e37e29214830a6
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-08 15:35:00 +0000

    add bg image

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -33,9 +33,7 @@ export default function Contacts() {
             </span>
           </p>
           <div>
-            <iframe
-              width="600"
-              height="450"
+            <iframe className="frame"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2537.1767743243076!2d30.455207375897714!3d50.51227757160405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d27e757d0d57%3A0x5f2eb88179a7c746!2sDubrovytska%20St%2C%2028%2C%20Kyiv%2C%20Ukraine%2C%2004114!5e0!3m2!1sen!2sus!4v1699447132214!5m2!1sen!2sus"
               allowfullscreen=""
               loading="lazy"
