39d731031aa4b2c5a72a5f54ab0f34e1d9b54d97
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-08 14:10:34 +0000

    update header style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import "../styles/layout.css";
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
+    <div>
       <Navbar />
       <div className="content"> {children}</div>
       <footer>
