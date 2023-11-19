ba2325309a2992a74e27378158f0b29212fc8de5
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-19 23:10:50 +0000

    add favicon and edit styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,20 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ТОВ ТЕКС-ІКА`,
+        short_name: `ТЕКС-ІКА`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+  ],
   siteMetadata: {
     title: "ТЕКС-ІКА",
   },
