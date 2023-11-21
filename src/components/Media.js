82a0f58cb03b64c2626fb5ead709560017bdde9f
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-21 11:58:21 +0000

    add-layer-to-video

--- src/components/Media.js
+++ src/components/Media.js
@@ -9,22 +9,42 @@ export default function Media() {
     <Card
       component="div"
       sx={{
+        position: "relative",
         minWidth: "220px",
         manWidth: "430px",
         height: "450px",
         padding: "0",
         flexGrow: 1,
         borderRadius: "26px",
+        overflow: "hidden",
         "@media screen and (max-width: 600px)": {
-          width: "200px"
+          width: "200px",
         },
       }}
     >
       <CardCover>
-        <video autoPlay loop muted poster={poster}>
+        <video
+          autoPlay
+          loop
+          muted
+          poster={poster}
+          style={{ width: "100%", height: "100%" }}
+        >
           <source src={intro} type="video/mp4" />
         </video>
       </CardCover>
+      <div
+        style={{
+          content: '""',
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          pointerEvents: "none",
+        }}
+      />
     </Card>
   );
 }
